9a436cdf5832300f11867038df886da06f574bf1
rgomes-d <rgomes-d@student.42sp.org.br>
2025-07-28 20:29:37 -0300

    feat: criação da gnl sem refactor.

--- get_next_line.c
+++ get_next_line.c
@@ -6,87 +6,218 @@
 /*   By: rgomes-d <rgomes-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 12:47:47 by rgomes-d          #+#    #+#             */
-/*   Updated: 2025/07/27 20:31:22 by rgomes-d         ###   ########.fr       */
+/*   Updated: 2025/07/28 20:26:59 by rgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	*get_next_line(int fd)
-{
-static int save_fd;
-static t_list *b_read;
-if(save_fd == 0)
-{
-	save_fd = fd;
-	b_read = Verify_line(fd);
-}
-if(!save_fd)
-	save_fd = 1;
-}
+char	*fill_line(t_list **lst);
+void	verify_line(char *str, t_list **stock, size_t bff_lido);
 
-t_list *Verify_line(int fd)
+char	*get_next_line(int fd)
 {
-	size_t BUFFER_SIZE = 42;
-	void *str;
-	t_list *list_content;
-	list_content = malloc(sizeof(t_list));
-	list_content->content = '\0';
-	t_list *head;
-	t_list *tail;
-	char *c = malloc(1);
-	int i = 0;
+	static int		save_fd = -1;
+	static t_list	*b_read;
+	void			*str;
+	t_list			*head;
+	t_list			*aux;
+	char			*rtn;
 
-	read(fd,str,BUFFER_SIZE);
-	while(i < BUFFER_SIZE)
+	str = NULL;
+	if (!b_read)
+		rtn =  NULL;
+	else
+		rtn = fill_line(&b_read);
+	if (save_fd == -1)
+		save_fd = fd;
+	while (rtn == NULL)
 	{
-		if (list_content->content == NULL)
+		str = malloc(BUFFER_SIZE);
+		int teste = (int)read(save_fd, str, BUFFER_SIZE);
+		if (teste <= 0)
 		{
+			free(str);
+			return (NULL);
+		}
+		verify_line(str, &b_read,teste);
+		free(str);
+		rtn = fill_line(&b_read);
+	}
+	head = b_read;
+	while (b_read && ((char *)b_read->content)[0] != '\0' && ((char *)b_read->content)[0] != '\n')
+		b_read = b_read->next;
+	if (b_read && ((char *)b_read->content)[0] == '\0')
+		while (b_read->next != NULL)
+			b_read = b_read->next;
+	else
+		b_read = b_read->next;
+	while(head != b_read)
+	{
+		aux = head;
+		head = head->next;
+		free(aux->content);
+		free(aux);
+	}
+	return (rtn);
+}
+
+void	verify_line(char *str, t_list **stock, size_t bff_lido)
+{
+	char	*c;
+	int		i;
+	t_list	*list_content;
+	t_list	*tail;
+
+	tail = *stock;
+	list_content = NULL;
+	if (tail)
+	{
+		while (tail->next)
+			tail = tail->next;
+	}
+	i = 0;
+	while (i < (int)bff_lido)
+	{
+		if (*stock == NULL)
+		{
+			c = malloc(1);
+			list_content = malloc(sizeof(t_list));
+			list_content->content = NULL;
 			c[0] = ((char *)str)[i];
 			list_content->content = (void *)c;
 			list_content->next = NULL;
-			tail = tail;
-			head = list_content;
+			*stock = list_content;
+			tail = list_content;
 		}
 		else
 		{
 			c = malloc(1);
+			if (!list_content)
+				list_content = tail;
 			c[0] = ((char *)str)[i];
 			tail = malloc(sizeof(t_list));
 			tail->content = (void *)c;
 			tail->next = NULL;
 			list_content->next = tail;
-			list_content = list_content->next;
+			list_content = tail;
+			tail = tail->next;
 		}
 		i++;
 	}
+}
 
+char	*fill_line(t_list **lst)
+{
+	t_list	*head;
+	size_t	loc;
+	size_t	i;
+	char	*rtn;
+	t_list	*tail = *lst;
 
+	loc = 0;
+	i = 0;
+	head = *lst;
+	while (head->next && ((char *)head->content)[0] != '\n' && ((char *)head->content)[0] != 0)
+	{
+		head = head->next;
+		loc++;
+	}
+	if (((char *)head->content)[0] != '\n' && ((char *)head->content)[0] != 0)
+		return (NULL);
+	else
+	{
+		rtn = malloc(loc + 2);
+		rtn[loc+1] = 0;
+		while (i <= loc)
+		{
+			rtn[i] = ((char *)tail->content)[0];
+			tail = tail->next;
+			i++;
+		}
+	}
+	return (rtn);
 }
 
 int	main(void)
 {
-	get_next_line(1);
-	get_next_line(1);
-	get_next_line(1);
-	get_next_line(1);
-	get_next_line(1);
-	return (0);
+	int fd;
+	char *teste;
 
-
-	list_content = head;
-	while (list_content)
-	{
-		printf("%c",((char *)list_content->content)[0]);
-		list_content = list_content->next;
-	}
-
-	while (head)
-	{
-		list_content = head;
-		head = head->next;
-		free(list_content->content);
-		free(list_content);
-	}
+	fd = open("teste.txt",0);
+	teste = get_next_line(0);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	teste = get_next_line(fd);
+	printf("%s",teste);
+	free(teste);
+	// teste = get_next_line(fd);
+	// printf("%s",teste);
+	// free(teste);
 }
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: rgomes-d <rgomes-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 14:12:48 by rgomes-d          #+#    #+#             */
-/*   Updated: 2025/07/27 20:15:54 by rgomes-d         ###   ########.fr       */
+/*   Updated: 2025/07/28 14:40:59 by rgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 2
+#endif
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -24,6 +28,6 @@ typedef struct s_list
 	struct s_list *next;
 } t_list;
 
-int	*get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
